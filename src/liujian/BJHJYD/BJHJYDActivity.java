55ce9e94651a54f442b6add7ae0975ec98b028ef
liujian <liujian3@gmail.com>
2012-05-10 20:50:51 +0800

    doc

--- src/liujian/BJHJYD/BJHJYDActivity.java
+++ src/liujian/BJHJYD/BJHJYDActivity.java
@@ -3,6 +3,12 @@ package liujian.BJHJYD;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * sb
+ *  @param [in] param1 
+ *  @param [out] param2 
+ *  @return r
+ */
 public class BJHJYDActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
