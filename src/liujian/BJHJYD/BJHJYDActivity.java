a146a937421cc7f071901b76be1d876c4c2c98d0
liujian <liujian3@gmail.com>
2012-05-10 21:43:17 +0800

    Signed-off-by: liujian <liujian3@gmail.com> sms

--- src/liujian/BJHJYD/BJHJYDActivity.java
+++ src/liujian/BJHJYD/BJHJYDActivity.java
@@ -1,19 +1,32 @@
 package liujian.BJHJYD;
 
 import android.app.Activity;
+import android.telephony.SmsManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
- * sb
- *  @param [in] param1 
- *  @param [out] param2 
- *  @return r
+ * create
  */
 public class BJHJYDActivity extends Activity {
+	TextView textview1;
+	Button b;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        textview1 = (TextView)findViewById(R.id.textView1);  
+        b=(Button)findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v)
+        	{
+        		SmsManager sm = SmsManager.getDefault();
+        		sm.sendTextMessage("13810249488", null, "asdf", null, null);
+        	}
+        });
     }
 }
