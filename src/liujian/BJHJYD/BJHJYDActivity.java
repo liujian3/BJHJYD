10e72b40d1206f1ff3c883a6ffec8697839ea75c
unknown <wj@wj-PC.(none)>
2012-07-31 00:16:49 +0800

    Signed-off-by: unknown <wj@wj-PC.(none)>

--- src/liujian/BJHJYD/BJHJYDActivity.java
+++ src/liujian/BJHJYD/BJHJYDActivity.java
@@ -1,32 +1,18 @@
 package liujian.BJHJYD;
 
 import android.app.Activity;
-import android.telephony.SmsManager;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.Window;
 
 /**
  * create
  */
 public class BJHJYDActivity extends Activity {
-	TextView textview1;
-	Button b;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-        textview1 = (TextView)findViewById(R.id.textView1);  
-        b=(Button)findViewById(R.id.button1);
-        b.setOnClickListener(new OnClickListener(){
-        	public void onClick(View v)
-        	{
-        		SmsManager sm = SmsManager.getDefault();
-        		sm.sendTextMessage("13810249488", null, "asdf", null, null);
-        	}
-        });
     }
 }
