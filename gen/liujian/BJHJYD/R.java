c4ed4757d6f6b48830ebd85960634d78d0c49e31
liujian <liujian3@gmail.com>
2012-07-31 19:32:37 +0800

    Signed-off-by: liujian <liujian3@gmail.com>

--- gen/liujian/BJHJYD/R.java
+++ gen/liujian/BJHJYD/R.java
@@ -14,11 +14,17 @@ public final class R {
         public static final int bkg=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
+    public static final class id {
+        public static final int btncommit=0x7f060002;
+        public static final int etcode=0x7f060001;
+        public static final int spdate=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int btncommitstr=0x7f040001;
     }
     public static final class style {
         public static final int notitle=0x7f050000;
--- src/liujian/BJHJYD/BJHJYDActivity.java
+++ src/liujian/BJHJYD/BJHJYDActivity.java
@@ -1,18 +1,71 @@
 package liujian.BJHJYD;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * create
  */
 public class BJHJYDActivity extends Activity {
     /** Called when the activity is first created. */
+	Button btnCommit;
+	EditText etCode;
+	Spinner spDate;
+	ArrayList<String> alDate;
+	ArrayAdapter<String> aaDate;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        
+    	btnCommit = (Button)findViewById(R.id.btncommit);
+    	etCode = (EditText)findViewById(R.id.etcode);
+    	spDate = (Spinner)findViewById(R.id.spdate);
+    	
+    	alDate = new ArrayList<String>();
+    	
+    	alDate.add("201207");
+    	alDate.add("201206");
+    	alDate.add("201205");
+    	alDate.add("201204");
+    	alDate.add("201203");
+    	alDate.add("201202");
+    	alDate.add("201201");
+    	alDate.add("201112");
+    	alDate.add("201111");
+    	alDate.add("201110");
+    	alDate.add("201109");
+    	alDate.add("201108");
+    	alDate.add("201107");
+    	alDate.add("201106");
+    	alDate.add("201105");
+    	alDate.add("201104");
+    	alDate.add("201103");
+    	alDate.add("201102");    	
+    	alDate.add("201101");    	
+    	
+    	aaDate = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,alDate);
+    	aaDate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    	spDate.setAdapter(aaDate);
+    	
+    	btnCommit.setOnClickListener(new Button.OnClickListener()
+    	{
+    		public void onClick(View v)
+    		{
+    			AlertDialog ad = new AlertDialog.Builder(BJHJYDActivity.this).setTitle("结果")	.
+    					setMessage("恭喜您！没中……").
+    					setIcon(R.drawable.ic_launcher).setNegativeButton("返回", null).
+    					create();
+    			ad.show();
+    		}
+    	});
     }
 }
